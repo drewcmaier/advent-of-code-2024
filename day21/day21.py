4669b5aa08e8c31d14d5ca39ea8f646c470e8e47
Drew Maier <drewcmaier@gmail.com>
2025-02-02 18:35:49 -0500

    Day 21: optimize for constant-time evaluation

--- day21/day21.py
+++ day21/day21.py
@@ -1,7 +1,13 @@
-import heapq
 from enum import Enum
 from functools import cache
-from itertools import permutations
+from paths import (
+    min_numpad_paths,
+    numpad_start_point,
+    numpad_locations,
+    min_directional_paths,
+    directional_start_point,
+    directional_locations,
+)
 
 type Point = tuple[int, int]
 type Grid = list[list[str]]
@@ -41,133 +47,6 @@ def parse_input(f: str):
         lines = file.readlines()
 
     return [line.strip() for line in lines]
-
-
-def get_button_locations(grid: Grid):
-    locations = dict[str, Point]()
-    for i, _ in enumerate(grid):
-        for j, button in enumerate(grid[i]):
-            locations[button] = (i, j)
-    return locations
-
-
-def can_move_to(pos: Point, grid: Grid) -> bool:
-    rows, cols = len(grid), len(grid[0])
-    if not (0 <= pos[0] < rows and 0 <= pos[1] < cols):
-        return False
-
-    return grid[pos[0]][pos[1]] != EMPTY
-
-
-def find_paths(start: Point, grid: Grid, costs: dict[str, int]):
-    def get_neighbors(p: Point):
-        neighbors: list[Move] = []
-        for d in Direction:
-            offset, symbol = d.value
-            d_pos = (offset[0] + p[0], offset[1] + p[1])
-            if not can_move_to(d_pos, grid):
-                continue
-            neighbors.append((d_pos, symbol))
-        return neighbors
-
-    # Dijkstra's algorithm
-    distances: dict[Point, int] = {start: 0}
-    paths: dict[Point, list[Move]] = {start: []}
-    q = [(0, start, "", [(start, "")])]
-    while q:
-        dist, pos, direction, path = heapq.heappop(q)
-
-        for neighbor in get_neighbors(pos):
-            neighbor_point, neighbor_direction = neighbor
-            neighbor_path = path + [(neighbor_point, neighbor_direction)]
-
-            turn_cost = (
-                costs[TURN] if direction and direction != neighbor_direction else 0
-            )
-
-            neighbor_cost = costs[neighbor_direction]
-            neighbor_score = dist + 1 + turn_cost + neighbor_cost
-
-            if neighbor_score < distances.get(neighbor_point, float("inf")):
-                distances[neighbor_point] = neighbor_score
-                paths[neighbor_point] = neighbor_path
-                heapq.heappush(
-                    q,
-                    (
-                        neighbor_score,
-                        neighbor_point,
-                        neighbor_direction,
-                        neighbor_path,
-                    ),
-                )
-
-    return paths
-
-
-def precompute_paths(
-    grid: Grid, costs: dict[str, int], prev_paths: dict[Point, dict[Point, list[Move]]]
-):
-    def num_turns(moves: list[Move]):
-        turns, heading = 0, ""
-        for move in moves:
-            _, d = move
-            if d != heading:
-                turns += 1
-            heading = d
-        return turns
-
-    # For each point, generate path to all other points
-    paths = prev_paths
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == EMPTY:
-                continue
-
-            point = (i, j)
-            paths_from_point = find_paths(point, grid, costs)
-            for other_point, path in paths_from_point.items():
-                if point in prev_paths and num_turns(
-                    prev_paths[point][other_point]
-                ) < num_turns(path):
-                    paths_from_point[other_point] = prev_paths[point][other_point]
-
-            paths[point] = paths_from_point
-
-    return paths
-
-
-def debug_move(move: list[Move], grid: Grid):
-    s = ""
-    rows, cols = len(grid), len(grid[0])
-    moves = dict[Point, str]()
-    for point, symbol in move:
-        moves[point] = symbol
-
-    for i in range(rows):
-        for j in range(cols):
-            s += grid[i][j] if (i, j) not in moves else "█"
-        s += "\n"
-    return s
-
-
-def debug_enter_buttons(buttons: str, start: Point, grid: Grid):
-    button_lookup = {}
-    for d in Direction:
-        direction, symbol = d.value
-        button_lookup[symbol] = direction
-    button_lookup[ACTIVATE] = ACTIVATE
-
-    s = ""
-    cur = start
-    for b in buttons:
-        if b == ACTIVATE:
-            s += grid[p[0]][p[1]]
-        else:
-            direction = button_lookup[b]
-            p = (cur[0] + direction[0], cur[1] + direction[1])
-            cur = p
-
-    return s
 
 
 def path_str(path: list[Move]):
@@ -228,21 +107,14 @@ def button_path_length(
 
 def simulate(
     iterations: int,
-    costs: dict[str, int] = {},
-    min_numpad_path: dict[Point, dict[Point, list[Move]]] = {},
-    min_directional_path: dict[Point, dict[Point, list[Move]]] = {},
+    numpad_paths: dict[Point, dict[Point, list[Move]]],
+    numpad_start_point: Point,
+    numpad_locations: dict[str, Point],
+    directional_paths: dict[Point, dict[Point, list[Move]]],
+    directional_start_point: Point,
+    directional_locations: dict[str, Point],
     dbg: bool = False,
 ):
-    numpad_paths = precompute_paths(NUMERIC_BUTTONS, costs, min_numpad_path)
-    numpad_locations = get_button_locations(NUMERIC_BUTTONS)
-    numpad_start_point = numpad_locations["A"]
-
-    directional_paths = precompute_paths(
-        DIRECTIONAL_BUTTONS, costs, min_directional_path
-    )
-    directional_locations = get_button_locations(DIRECTIONAL_BUTTONS)
-    directional_start_point = directional_locations["A"]
-
     complexity = []
     numpad_instructions = parse_input("test.txt" if dbg else "input.txt")
     directional_instructions: list[str] = []
@@ -264,33 +136,32 @@ def simulate(
         )
         complexity[i] *= length
 
-    return (sum(complexity), numpad_paths, directional_paths)
+    return sum(complexity)
 
 
-def part1(iterations: int):
-    numpad_paths, directional_paths = {}, {}
-    min_complexity, min_params = float("inf"), {}
-
-    # Tune Dijkstra parameters to find min paths
-    for d, u, t in permutations(range(10), 3):
-        params = {LEFT: 1, RIGHT: 3, UP: u, DOWN: d, TURN: t}
-        complexity, numpad_paths, directional_paths = simulate(
-            iterations, params, numpad_paths, directional_paths
-        )
-        if complexity < min_complexity:
-            min_complexity = complexity
-            min_params = params
-
-    # print(numpad_paths)
-    # print(directional_paths)
-
-    simulate(iterations, min_params, numpad_paths, directional_paths, dbg=False)
-    print(f"Answer: {min_complexity}")
-    return (min_params, numpad_paths, directional_paths)
+def part1():
+    complexity = simulate(
+        2,
+        min_numpad_paths,
+        numpad_start_point,
+        numpad_locations,
+        min_directional_paths,
+        directional_start_point,
+        directional_locations,
+        dbg=False,
+    )
+    return complexity
 
 
 def part2():
-    return part1(25)
-
-
-part2()
+    complexity = simulate(
+        25,
+        min_numpad_paths,
+        numpad_start_point,
+        numpad_locations,
+        min_directional_paths,
+        directional_start_point,
+        directional_locations,
+        dbg=False,
+    )
+    return complexity
